704c102c5c88a812b54dfbae117b635b0746636c
Vikram Khajuria <224464006+Vikramkhajuria@users.noreply.github.com>
2025-12-02 17:57:06 +0100

    Implement TrafficAnalyzer with weather and congestion risk logic

--- src/App.java
+++ src/App.java
@@ -1,15 +1,28 @@
-public class App{
-    public static void main(String[] args){
+public class App {
+    public static void main(String[] args) {
 
         Location location = new Location("Stockholm");
-        System.out.println(location.getName());
+//        System.out.println(location.getName());
 
-        WeatherInfo weather = new WeatherInfo(30, "Raining");
-        System.out.println(weather.getTemperature());
-        System.out.println(weather.getCondition());
+        WeatherInfo weather1 = new WeatherInfo(0, "Snow");
+//        System.out.println(weather.getTemperature());
+//        System.out.println(weather.getCondition());
 
-        TrafficInfo trafficInfo = new TrafficInfo(8);
-        System.out.println(trafficInfo.getCongestionLevel());
+        TrafficInfo trafficInfo1 = new TrafficInfo(9);
+//        System.out.println(trafficInfo.getCongestionLevel());
+
+        WeatherInfo weather2 = new WeatherInfo(15, "clear");
+        TrafficInfo trafficInfo2 = new TrafficInfo(7);
+
+        WeatherInfo weather3 = new WeatherInfo(20, "Sunny");
+        TrafficInfo trafficInfo3 = new TrafficInfo(3);
+
+
+        System.out.println(TrafficAnalyzer.getRiskLevel(weather1, trafficInfo1));
+        System.out.println(TrafficAnalyzer.getRiskLevel(weather2, trafficInfo2));
+        System.out.println(TrafficAnalyzer.getRiskLevel(weather3, trafficInfo3));
+
+
 
     }
 }
---
+++ src/TrafficAnalyzer.java
@@ -0,0 +1,16 @@
+public class TrafficAnalyzer {
+
+    public static String getRiskLevel(WeatherInfo Weather1, TrafficInfo trafficInfo1) {
+        String condition = Weather1.getCondition();
+        int congestion = trafficInfo1.getCongestionLevel();
+
+        if (congestion >= 8 && condition.equals("Snow")) {
+            return "High";
+        } else if (congestion >= 6) {
+            return "medium";
+
+        } else {
+            return "Low";
+        }
+    }
+}
