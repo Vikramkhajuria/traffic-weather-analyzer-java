625f65dafcd6eb9eb7c8e01c40391f775c11d0e0
Vikram Khajuria <224464006+Vikramkhajuria@users.noreply.github.com>
2025-12-03 23:26:05 +0100

    Populate FakeDataService with sample locations, weather, and traffic data

--- src/FakeDataService.java
+++ src/FakeDataService.java
@@ -13,7 +13,47 @@ public class FakeDataService {
         locations = new ArrayList<>();
         weatherMap = new HashMap<>();
         trafficMap = new HashMap<>();
-    }
 
+        // Location 1
+        Location stockholm = new Location("Stockholm Central");
+        locations.add(stockholm);
+
+        WeatherInfo stockholmWeather = new WeatherInfo(0, "Snow");
+        TrafficInfo stockholmTraffic = new TrafficInfo(9);
+
+        weatherMap.put(stockholm.getName(), stockholmWeather);
+        trafficMap.put(stockholm.getName(), stockholmTraffic);
+
+        // Location 2
+        Location malmo = new Location("Malmö Center");
+        locations.add(malmo);
+
+        WeatherInfo malmoWeather = new WeatherInfo(14, "Cloudy");
+        TrafficInfo malmoTraffic = new TrafficInfo(5);
+
+        weatherMap.put(malmo.getName(), malmoWeather);
+        trafficMap.put(malmo.getName(), malmoTraffic);
+
+        // Location 3
+        Location e4 = new Location("E4 Northbound");
+        locations.add(e4);
+
+        WeatherInfo e4Weather = new WeatherInfo(5, "Rain");
+        TrafficInfo e4Traffic = new TrafficInfo(7);
+
+        weatherMap.put(e4.getName(), e4Weather);
+        trafficMap.put(e4.getName(), e4Traffic);
+
+        // Location 4
+        Location goth = new Location("Gothenburg Ring Road");
+        locations.add(goth);
+
+        WeatherInfo gothWeather = new WeatherInfo(10, "Clear");
+        TrafficInfo gothTraffic = new TrafficInfo(3);
+
+        weatherMap.put(goth.getName(), gothWeather);
+        trafficMap.put(goth.getName(), gothTraffic);
+
+    }
 
 }
